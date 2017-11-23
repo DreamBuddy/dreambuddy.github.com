152abc8d683eeb1d1e8d3572d831b2da300e8df9
Xu Mengtong <mt@xumengtongdeMacBook-Pro.local>
2017-11-24 02:04:49 +0800

    切换 简历数据源到本地

--- MyResume/data.php
+++ MyResume/data.php
@@ -1,8 +1,8 @@
 <?php
 
-$viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
-$title = '标题';
-$subtitle = '子标题';
+$viewpass = ''; // 密码必须大于3位。留空则任何人可以访问
+$title = '徐梦通'; // 标题
+$subtitle = ''; // 子标题
 $content = 'MarkDown';
 
 $data['local'] = 1;
